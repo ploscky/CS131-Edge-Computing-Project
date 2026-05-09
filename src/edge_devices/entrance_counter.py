7dbb52008fdcf381c11cf6912a2132ad957eeeb9
Selina Wu <selinawu@Selinas-MacBook-Air.local>
2026-05-09 11:07:32 -0700

    test code for yolo and supervision model to track entrances

--- src/edge_devices/entrance_counter.py
+++ src/edge_devices/entrance_counter.py
@@ -4,6 +4,9 @@ import sys
 import time
 from datetime import datetime, timezone
 from pathlib import Path
+import cv2
+from ultralytics import YOLO
+import supervision as sv
 
 import zmq
 
@@ -12,16 +15,52 @@ sys.path.append(str(Path(__file__).resolve().parents[1]))
 
 from config import ENTRANCE_DEVICE_ID, ENTRANCE_UPDATE_SECONDS, FOG_SUB_CONNECT
 
+model = YOLO("yolov8n.pt") # download yolo model
+cap = cv2.VideoCapture(0) # update later for jetson camera
+
+bound_start = sv.Point(300, 0) # adjust coordinates later for actual doorway
+bound_end = sv.Point(300, 640)
+people_counter = sv.LineZone(start=bound_start, end=bound_end) # tracks # of people crossing doorway
+bound_annotator = sv.LineZoneAnnotator()
+box_annotator = sv.BoxAnnotator()
+tracker = sv.ByteTrack() # make sure a person isn't counted twice w/ ID
+
+# helps track how many people come in per frame
+prev_in = 0
+prev_out = 0
 
 def simulate_entrance_event() -> int:
     # Temporary stand-in for the camera/person tracking code.
     # Positive means someone walked in, negative means someone walked out.
-    return random.choices(
-        population=[1, -1, 0],
-        weights=[0.35, 0.20, 0.45],
-        k=1,
-    )[0]
+    # return random.choices(
+    #     population=[1, -1, 0],
+    #     weights=[0.35, 0.20, 0.45],
+    #     k=1,
+    # )[0]
+    global prev_in, prev_out
 
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed frame grab")
+        return 0
+    
+    # use supervision to check trajectory of movement (in or out) 
+    results = model(frame, classes=[0])[0] # only detect people
+    detections = sv.Detections.from_ultralytics(results)
+    detections = tracker.update_with_detections(detections)
+    people_counter.trigger(detections=detections)
+    
+    # show vid feed and boxes for testing
+    frame = box_annotator.annotate(scene=frame, detections=detections)
+    bound_annotator.annotate(frame, line_counter=people_counter)
+    cv2.imshow("Video Feed", frame) 
+    cv2.waitKey(1) # for vid playback
+
+    # compute number of people that entered/exited since last frame grab
+    people_in_building = (people_counter.in_count - prev_in) - (people_counter.out_count - prev_out)
+    prev_in = people_counter.in_count 
+    prev_out = people_counter.out_count
+    return people_in_building
 
 def now_iso() -> str:
     return datetime.now(timezone.utc).isoformat()
