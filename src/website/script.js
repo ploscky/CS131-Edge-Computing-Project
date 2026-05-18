d17be47937646c32adbb9b8d6008708e60ec599e
Andres-55 <andressolorio48@gmail.com>
2026-05-17 22:17:21 -0700

    Updated website to look cleaner and added a status for how busy it is

--- src/website/script.js
+++ src/website/script.js
@@ -8,6 +8,7 @@ async function loadDashboard() {
         document.getElementById("waiting").innerText = data.waiting;
         document.getElementById("seated").innerText = data.seated;
         document.getElementById("waitTime").innerText = data.estimatedWaitTime;
+        document.getElementById("busyStatus").innerText = data.busyStatus;
 
         const bestTime = `${data.bestTime.start} - ${data.bestTime.end} ${data.bestTime.time}`;
         document.getElementById("bestTime").innerText = bestTime;
@@ -26,11 +27,39 @@ async function loadDashboard() {
             tableContainer.appendChild(div);
         });
 
+        const status = data.busyStatus.toLowerCase();
+        const el = document.getElementById("busyStatus");
+
+        el.innerText = status;
+
+        el.classList.remove("not-busy", "busy", "very-busy");
+        el.classList.add(status.replace(" ", "-"));
     } catch (error) {
         console.error("Error loading dashboard", error);
     }
 }
 
+function updateDateTime() {
+    const now = new Date();
+
+    const options = {
+        month: "long",
+        day: "numeric"
+    };
+
+    const date = now.toLocaleDateString("en-US", options);
+
+    const time = now.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit"
+    });
+
+    document.getElementById("date").innerText = date;
+    document.getElementById("time").innerText = time;
+}
+
 loadDashboard();
+updateDateTime();
 
 setInterval(loadDashboard, 2000);
+setInterval(updateDateTime, 3000);
