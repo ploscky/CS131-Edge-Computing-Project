cd14a9cd74a6587e982a5cf16db34836e6678e2f
Andres-55 <andressolorio48@gmail.com>
2026-05-24 23:16:17 -0700

    Fixed the seats.py file to ID each zone, track amount of people in each zone, and update the table occupancy on website

--- src/edge_devices/seats.py
+++ src/edge_devices/seats.py
@@ -28,8 +28,18 @@ cap.set(cv2.CAP_PROP_BUFFERSIZE, 1) # get most recent frame
 # define seat zones as rectangles using Polygon (will adjust later for actual coordinates)
 # find actual coordinates by taking screenshot of camera feed and finding pixel coords
 SEAT_ZONES = [
-    sv.PolygonZone(polygon=np.array([[100, 100], [300, 100], [300, 300], [100, 300]])), # placeholder coords
-    sv.PolygonZone(polygon=np.array([[350, 100], [550, 100], [550, 300], [350, 300]]))
+    {
+        "id": 1,
+        "polygon": np.array([[100, 100], [300, 100], [300, 300], [100, 300]], dtype=np.int32) 
+    },
+    {
+        "id": 2, 
+        "polygon": np.array([[350, 100], [550, 100], [550, 300], [350, 300]], dtype=np.int32)
+    },
+    {
+        "id": 3, 
+        "polygon": np.array([[650, 400], [850, 400], [850, 600], [650, 600]], dtype=np.int32)
+    }
     # add more zones later
 ]
 
@@ -40,9 +50,8 @@ frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 print(f"Resolution: {frame_width}x{frame_height}")
 
 # show boxes for testing
-box_annotator = sv.BoxAnnotator()
 # draw green polygons around zones
-zone_annotators = [sv.PolygonZoneAnnotator(zone=zone, color=sv.Color.GREEN) for zone in SEAT_ZONES]
+zone_annotators = [sv.PolygonZoneAnnotator(zone=sv.PolygonZone(polygon=zone["polygon"]), color=sv.Color.GREEN, text_scale=0) for zone in SEAT_ZONES]
 
 # resize frame -- use this if latency is an issue
 # def get_resized_frame():
@@ -56,7 +65,7 @@ def occupied_seats() -> int:
     ret, frame = cap.read()
     if not ret:
         print("Failed frame grab")
-        return 0
+        return {}
     # frame = get_resized_frame()
     # if frame is None:
     #     print("Failed frame grab")
@@ -65,16 +74,37 @@ def occupied_seats() -> int:
     results = model(frame, classes=[0])[0] # only detect people
     detections = sv.Detections.from_ultralytics(results)
 
-    frame = box_annotator.annotate(scene=frame, detections=detections)
+    zone_counts = {}
 
-    occupied = 0
+    #Counts how many people are in each zone
     for zone, zone_annotator in zip(SEAT_ZONES, zone_annotators):
-        zone.trigger(detections=detections)
-
         frame = zone_annotator.annotate(scene=frame)
 
-        if zone.current_count >= 1:
-            occupied += 1
+        people_in_zone = 0
+        for xyxy in detections.xyxy:
+            x1, y1, x2, y2 = xyxy
+
+            cx = int((x1+x2) / 2)
+            cy = int((y1+y2) / 2)
+
+            result = cv2.pointPolygonTest(zone["polygon"], (cx,cy), False)
+
+            if result >= 0:
+                cv2.circle(frame, (cx, cy), 8, (0,255,0), -1)
+
+            else:
+                cv2.circle(frame, (cx, cy), 6, (0, 0, 255), -1)
+
+            if result >= 0:
+                people_in_zone += 1
+        zone_counts[zone["id"]] = people_in_zone
+
+        label_x = zone["polygon"][0][0]
+        label_y = zone["polygon"][0][1] - 10
+
+        count = people_in_zone
+
+        cv2.putText(frame, f"People: {count}", (label_x, label_y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
     
     # show vid feed and boxes for testing
     cv2.imshow("Video Feed", frame) 
@@ -82,7 +112,7 @@ def occupied_seats() -> int:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         return -999 # returns when q is pressed during stream
     
-    return occupied
+    return zone_counts
 
 def now_iso() -> str:
     return datetime.now(timezone.utc).isoformat()
@@ -102,9 +132,9 @@ def main():
     print(f"[{SEAT_DEVICE_ID}] sending seating updates to fog")
 
     while True:
-        occupied = occupied_seats()
+        zone_counts = occupied_seats()
 
-        if occupied == -999: 
+        if zone_counts == -999: 
             break # exiting on 'q'
 
         # send ZMQ messages every 2 seconds
@@ -112,7 +142,7 @@ def main():
             # Keeping the message as basic JSON for now so it is easy to inspect.
             message = {
                 "device_id": SEAT_DEVICE_ID,
-                "number_of_occupied_seats": occupied,
+                "zones": zone_counts,
                 "timestamp": now_iso(),
             }
 
@@ -125,7 +155,6 @@ def main():
     # clean up data
     cap.release()
     cv2.destroyAllWindows()   
-
 
 if __name__ == "__main__":
     main()
